4682cf3cb33b263b5bdf11b82c81df6a54ff7fca
talnmizrachi <tal.mizrachi@masterschool.com>
2023-03-20 15:00:08 +0200

    adding logger error for file movement

--- slack_bot/file_handler.py
+++ slack_bot/file_handler.py
@@ -29,9 +29,9 @@ it's ok."""
 
 def send_msg_to_sxm(client, student, company, poc_name):
 	managers = {"tal": 'U02SC4T1EBF',
-	            "anisa_": "U049VQ962ER",
-	            "ori_": "U030GUZ79NX"
-	            }
+				"anisa_": "U049VQ962ER",
+				"ori_": "U030GUZ79NX"
+				}
 	for manager, slack_id in managers.items():
 		text = f"<@{student}> sent their resumes to {poc_name} that knows someone at {company} on {datetime.now().strftime('%Y-%m-%d %H:%M')}"
 		client.chat_postMessage(text=text, channel=slack_id)
@@ -42,34 +42,39 @@ def interim_slack_handler(file_id, user_id, file_token):
 	file_data = client.files_info(file=file_id).data['file']
 	file_name = file_data['name']
 	file_link = file_data['permalink']
-	thread_ts = list(file_data['shares']['private'].values())[0][0]['thread_ts']
+	try:
+		thread_ts = list(file_data['shares']['private'].values())[0][0]['thread_ts']
+	except KeyError as e:
+		logger.error(file_data['shares']['private'].values())
+		logger.error(e)
+
 	session_ = session_generator()
 	hook_id, company, student_mail, full_name = (session_.
-	                                             query(CommunicationsModel).
-	                                             with_entities(CommunicationsModel.hook_id,
-	                                                           CommunicationsModel.company,
-	                                                           CommunicationsModel.student_email,
-	                                                           CommunicationsModel.full_name
-	                                                           ).
-	                                             filter_by(student_slack_id=user_id, thread_ts=thread_ts)
-	                                             .first()
-	                                             )
+												 query(CommunicationsModel).
+												 with_entities(CommunicationsModel.hook_id,
+															   CommunicationsModel.company,
+															   CommunicationsModel.student_email,
+															   CommunicationsModel.full_name
+															   ).
+												 filter_by(student_slack_id=user_id, thread_ts=thread_ts)
+												 .first()
+												 )
 	logger.debug(f"{hook_id} -Interim Slack Handler")
 	poc_name, poc_slack_id, connection_name = (session_.
-	                                           query(CommunicationsModel).
-	                                           with_entities(CommunicationsModel.poc_name,
-	                                                         CommunicationsModel.poc_slack_id,
-	                                                         CommunicationsModel.approved_connection_name
-	                                                         ).
-	                                           filter_by(hook_id=hook_id, message_type='CONNECTION_CONFIRMED')
-	                                           .first()
-	                                           )
+											   query(CommunicationsModel).
+											   with_entities(CommunicationsModel.poc_name,
+															 CommunicationsModel.poc_slack_id,
+															 CommunicationsModel.approved_connection_name
+															 ).
+											   filter_by(hook_id=hook_id, message_type='CONNECTION_CONFIRMED')
+											   .first()
+											   )
 	logger.debug(f"{hook_id} - getting information for the poc")
 	new_communications = CommunicationsModel(hook_id=hook_id, thread_ts=thread_ts, event="MSG_FROM_STUDENT",
-	                                         message_type="STUDENT_SENT_CV", company=company,
-	                                         student_email=student_mail, full_name=full_name, cv_link=file_link,
-	                                         file_name=file_name, student_slack_id=user_id, slack_file_token=file_token,
-	                                         poc_name=poc_name, poc_slack_id=poc_slack_id, job_id=None)
+											 message_type="STUDENT_SENT_CV", company=company,
+											 student_email=student_mail, full_name=full_name, cv_link=file_link,
+											 file_name=file_name, student_slack_id=user_id, slack_file_token=file_token,
+											 poc_name=poc_name, poc_slack_id=poc_slack_id, job_id=None)
 
 	try:
 		logger.debug(f"{hook_id} -Adding the new communication to the database")
@@ -83,23 +88,23 @@ def interim_slack_handler(file_id, user_id, file_token):
 	logger.debug(f"{hook_id} -Getting the job title")
 
 	job_title = (session_.
-	             query(WebhooksModel).
-	             with_entities(WebhooksModel.job_title).
-	             filter_by(hook_id=hook_id).
-	             first()
-	             )
+				 query(WebhooksModel).
+				 with_entities(WebhooksModel.job_title).
+				 filter_by(hook_id=hook_id).
+				 first()
+				 )
 
 	client.chat_postMessage(channel=poc_slack_id,
-	                        text=create_text_for_poc(company, connection_name, job_title, file_link))
+							text=create_text_for_poc(company, connection_name, job_title, file_link))
 
 	send_msg_to_sxm(client, user_id, company, poc_name)
 
 	new_communications = CommunicationsModel(hook_id=hook_id, thread_ts=thread_ts, event="MSG_TO_POC",
-	                                         message_type="ASTRID_FORWARDED_TO_POC", company=company,
-	                                         student_email=student_mail, full_name=full_name, cv_link=file_link,
-	                                         file_name=file_name, student_slack_id=user_id, slack_file_token=file_token,
-	                                         poc_name=poc_name, poc_slack_id=poc_slack_id,
-	                                         approved_connection_name=connection_name, job_id=None)
+											 message_type="ASTRID_FORWARDED_TO_POC", company=company,
+											 student_email=student_mail, full_name=full_name, cv_link=file_link,
+											 file_name=file_name, student_slack_id=user_id, slack_file_token=file_token,
+											 poc_name=poc_name, poc_slack_id=poc_slack_id,
+											 approved_connection_name=connection_name, job_id=None)
 	try:
 		session_.add(new_communications)
 		session_.commit()
@@ -122,6 +127,7 @@ def session_generator():
 
 
 def main_file_sender(data):
+	logger.debug(f"file_data:{data}")
 	event = data.get("event")
 	file = event['file']
 	file_id = file["id"]
@@ -132,7 +138,8 @@ def main_file_sender(data):
 	logger.debug(f"Received event:\t{event}")
 	logger.debug(f"Received file_id:\t{file_id}")
 
-	matchmaker = interim_slack_handler(file_id, user_id, file_token)
+	interim_slack_handler(file_id, user_id, file_token)
+
 
 
 @blueprint.route("/slack_events/")
@@ -161,18 +168,18 @@ class SlackEventCatcher(MethodView):
 if __name__ == '__main__':
 	session = session_generator()
 	details = (session.
-	           query(CommunicationsModel).
-	           with_entities(CommunicationsModel.hook_id,
-	                         CommunicationsModel.company,
-	                         CommunicationsModel.poc_name,
-	                         CommunicationsModel.event,
-	                         CommunicationsModel.thread_ts,
-	                         CommunicationsModel.message_type,
-	                         CommunicationsModel.student_email).
-	           filter_by(student_slack_id='U02SC4T1EBF',
-	                     # thread_ts=event_ts,
-	                     )
-	           .all()
-	           )
+			   query(CommunicationsModel).
+			   with_entities(CommunicationsModel.hook_id,
+							 CommunicationsModel.company,
+							 CommunicationsModel.poc_name,
+							 CommunicationsModel.event,
+							 CommunicationsModel.thread_ts,
+							 CommunicationsModel.message_type,
+							 CommunicationsModel.student_email).
+			   filter_by(student_slack_id='U02SC4T1EBF',
+						 # thread_ts=event_ts,
+						 )
+			   .all()
+			   )
 	print([d for d in details if d[4] == '1677838936.286059'])
 	print(f"Collected all the details:\t{details}")
