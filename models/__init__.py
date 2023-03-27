155d8bc9ae06a3853dde2267eada0087adad6e1c
talnmizrachi <tal.mizrachi@masterschool.com>
2023-03-28 00:23:50 +0300

    Adding new JobMovement object

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from models.communications import CommunicationsModel
 from models.connections import ConnectionModel
 from models.poc_slack_ids import POCSlackIDsModel
 from models.student_slack_ids import StudentSlackIDsModel
+from models.huntr_job_mvmnt import HuntrJobMovmentModel
